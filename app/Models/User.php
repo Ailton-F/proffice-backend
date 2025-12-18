5041483aef2d06e3000355226663c5cfc0999df5
Ailton-F <ailtonxdz@gmail.com>
2025-12-17 22:23:50 -0300

    refactor: relationships in the user model with class plans and subscriptions

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Subscription;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -53,5 +54,20 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     *  Get the class plans for the user.
+     */
+    public function class_plans()
+    {
+        return $this->hasMany(ClassPlan::class);
+    }
+
+    /**
+     * Get the subscriptions for the user.
+     */
+    public function subscriptions(){
+        return $this->hasMany(Subscription::class);
     }
 }
